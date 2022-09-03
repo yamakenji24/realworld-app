e9feeb3a786b60866d04c0682e81146511b556db
yamakenji24 <45092341+yamakenji24@users.noreply.github.com>
2022-09-03 23:46:47 +0900

    moved to features (#8)

--- astro/src/features/RegisterContent/RegisterForm.tsx
+++ astro/src/features/RegisterContent/RegisterForm.tsx
@@ -1,33 +1,36 @@
-import type { ReactElement, FormEvent } from "react";
+import type { FormEvent } from "react";
 import { useCallback } from "react";
-import { loginUserService } from "../../services/login-user";
-import { Input } from "../Input";
+import { registerUserService } from "../../services/register-user";
+import { Input } from "../../components/Input";
 
-export const LoginForm = (): ReactElement => {
+export const RegisterForm = () => {
   const handleOnSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const target = e.target as typeof e.target & {
+      username: { value: string };
       email: { value: string };
       password: { value: string };
     };
-    loginUserService({
+    registerUserService({
+      username: target.username.value,
       email: target.email.value,
-      password: target.password.value
-    })
+      password: target.password.value,
+    });
   }, []);
 
   return (
     <div className="col-md-6 offset-md-3 col-xs-12">
-      <h1 className="text-xs-center">Sign in</h1>
+      <h1 className="text-xs-center">Sign up</h1>
       <p className="text-xs-center">
-        <a href="/register">Need an account?</a>
+        <a href="/login">Have an account?</a>
       </p>
       <form onSubmit={handleOnSubmit}>
+        <Input type="text" name="username" placeholder="Your Name" />
         <Input type="text" name="email" placeholder="Email" />
         <Input type="password" name="password" placeholder="Password" />
 
         <button className="btn btn-lg btn-primary pull-xs-right">
-          Sign in
+          Sign up
         </button>
       </form>
     </div>
